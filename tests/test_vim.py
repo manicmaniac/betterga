ac1090a700c662c13470ac5abc593cf1cc2bb89d
Ryosuke Ito <rito.0305@gmail.com>
2015-07-29 20:55:50 +0900

    fix a test to avoid global pollution in py26

--- tests/test_vim.py
+++ tests/test_vim.py
@@ -5,9 +5,28 @@ from __future__ import unicode_literals
 import unittest
 import os
 import subprocess
+import vimrunner
 
 
-if not hasattr(subprocess, 'check_output'):
+class TestVim(unittest.TestCase):
+    def setUp(self):
+        if not hasattr(subprocess, 'check_output'):
+            subprocess.check_output = self.check_output
+        self.server = vimrunner.Server(extra_args=['-N', '-i', 'NONE'])
+        self.client = self.server.start()
+        self.client.add_plugin(os.getcwd(), 'betterga.vim')
+        self.client.write_buffer("1", 'a')
+
+    def tearDown(self):
+        self.server.quit()
+        if subprocess.check_output == self.check_output:
+            del subprocess.check_output
+
+    def testFunctionAscii(self):
+        self.assertEquals(self.client.echo('betterga#ascii()'),
+            '<a> [LATIN SMALL LETTER A] 97, Hex 0x61, Octal 0141\n0')
+
+    @staticmethod
     def check_output(*popenargs, **kwargs):
         if 'stdout' in kwargs:
             raise ValueError('stdout argument not allowed, it will be overridden.')
@@ -20,21 +39,4 @@ if not hasattr(subprocess, 'check_output'):
                 cmd = popenargs[0]
             raise subprocess.CalledProcessError(retcode, cmd, output=output)
         return output
-    subprocess.check_output = check_output
-import vimrunner
-
-
-class TestVim(unittest.TestCase):
-    def setUp(self):
-        self.server = vimrunner.Server(extra_args=['-N', '-i', 'NONE'])
-        self.client = self.server.start()
-        self.client.add_plugin(os.getcwd(), 'betterga.vim')
-        self.client.write_buffer("1", 'a')
-
-    def tearDown(self):
-        self.server.quit()
-
-    def testFunctionAscii(self):
-        self.assertEquals(self.client.echo('betterga#ascii()'),
-            '<a> [LATIN SMALL LETTER A] 97, Hex 0x61, Octal 0141\n0')
 
